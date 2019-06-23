7431af4416356a8ded107a4aaabfb5b2e0bb3575
Farkhod <sadykovfarkhod@gmail.com>
2019-06-22 21:28:23 -0500

    Deleted don't need folders

--- fuchicorp_webside/settings.py
+++ fuchicorp_webside/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'fuchicorp_webside.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- fuchicorp_webside/urls.py
+++ fuchicorp_webside/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('', include('contact.urls')),
-    path('', include('login.urls')),
 ]
