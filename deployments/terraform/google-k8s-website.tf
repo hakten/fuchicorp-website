19d6858c4dbed30fcc00387cb9da60809561b1c8
tunji57 <tunji57@gmail.com>
2020-03-10 20:04:24 -0500

    removed ./

--- deployments/terraform/google-k8s-website.tf
+++ deployments/terraform/google-k8s-website.tf
@@ -4,7 +4,7 @@ module "fuchicorp_website" {
   deployment_name        = "fuchicorp-website-${var.deployment_environment}"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
-  deployment_path        = "./website"
+  deployment_path        = "website"
   
   template_custom_vars  = {
     deployment_image  = "${var.deployment_image}"
